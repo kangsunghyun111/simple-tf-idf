a3515a5f3f92ece5e735c1f35a9f22eabb841322
kangsunghyun111 <tjdgu3408@naver.com>
2022-06-26 17:51:49 +0900

    simple tf-idf

--- README.md
+++ README.md
@@ -6,16 +6,39 @@ use after install mecab-ya
 
 https://github.com/golbin/node-mecab-ya
 
-## simple-tf-idf.js
+## simple-tf-idf-test.js
 ```JavaScript
-let simple_tfidf = require('./simple-tf-idf')   
+let simple_tfidf_test = require('./simple-tf-idf-test')
 
-let document = [];   
-document.push('정부가 발표하는 물가상승률과 소비자가 느끼는 물가상승률은 다르다.');   
-document.push('소비자는 주로 소비하는 상품을 기준으로 물가상승률을 느낀다.');   
+let document = [];
+document.push('정부가 발표하는 물가상승률과 소비자가 느끼는 물가상승률은 다르다.');
+document.push('소비자는 주로 소비하는 상품을 기준으로 물가상승률을 느낀다.');
 
-simple_tfidf.similarity_test(document);   
+simple_tfidf_test.similarity_test(document);
 ```
 
 0번 문서와 다른 문서의 유사도를 구하는 과정을 출력합니다.   
 과정을 살펴보기 위한 것이므로 입력은 5개 이하의 짧은 문장들이 좋습니다.   
+
+
+## simple-tf-idf.js
+```JavaScript
+let simple_tfidf = require('./simple-tf-idf')
+
+simple_tfidf.similarity_test(document);
+```
+
+0번 문서와 다른 문서를 비교해서 유사도가 높은 5개 문서를 출력합니다.   
+문서의 크기와 갯수가 커질수록 느려집니다. 또한 하나의 문서를 토큰화하는 시간이 4ms로, 1000개 문서의 경우 4s가 걸립니다.   
+
+
+```JavaScript
+let simple_tfidf = require('./simple-tf-idf')
+
+let tokenized_document = simpleTfidf.load_document_file('your_tokenized_file_path');
+
+simple_tfidf.similarity_test_token(tokenized_document);
+```
+
+토큰화한 데이터를 파일에 저장해서 관리한다면 시간이 많이 줄어듭니다.   
+토큰화 파일을 사용하면 10k 개의 문장을 비교하는데 200~300ms가 걸립니다.   
--- simple-tf-idf-test.js
+++ simple-tf-idf-test.js
@@ -36,44 +36,22 @@ function build_bag_of_words(tokenized_document){
     }
     
     for(let index in tokenized_document){
-        let bow_obj = {};
         let bow_temp = [];
+        bow_temp.length = word_to_index.size;
+        bow_temp.fill(0);
         
-        for(let word in tokenized_document[index]){
+        // 개별 문서의 BOW 구하기(tf 구하기)
+        for(let word in bow_temp){
             let i = word_to_index.get(tokenized_document[index][word]);
-            if(bow_temp.length === 0){
-                let pair = {
-                    index: i,
-                    value: 1
-                };
-                bow_temp.push(pair);
-            }
-            else{
-                let flag = 0;
-                for(let k in bow_temp){
-                    if(bow_temp[k].index === i){
-                        bow_temp[k].value += 1;
-                        flag = 1;
-                        break;
-                    }
-                }
-                
-                if(flag === 0){
-                    let pair = {
-                        index: i,
-                        value: 1
-                    };
-                    bow_temp.push(pair);
-                }
-            }
+            bow_temp[i] = bow_temp[i] + 1;
         }
         
+        // NaN 제거
+        bow_temp = bow_temp.filter(function(item){
+          return item !== null && item !== undefined && item !== '';
+        });
         
-        bow_obj = {
-            id: index,
-            bow: bow_temp
-        };
-        bow.push(bow_obj);
+        bow.push(bow_temp);
     }
     
     console.log('vocabulary : ', word_to_index);
@@ -82,22 +60,24 @@ function build_bag_of_words(tokenized_document){
     return [word_to_index, bow];
 }
 
-function get_idf(bow, vocab){
+function get_idf(bow){
     let df = [];
-    df.length = vocab.size;
+    df.length = bow[0].length;
     df.fill(0);
     
     // df 구하기
-    for(let i in bow){// 문서 개수만큼
-        for(let j in bow[i].bow){// 문서당 bow의 객체 개수만큼
-            df[bow[i].bow[j].index] += 1;
+    for(let i in df){
+        for(let index in bow){
+            if(bow[index][i] !== 0){
+                df[i] += 1;
+            }
         }
     }
     console.log('document frequency : ', df);
 
     let idf = [];
     let N = bow.length; // 전체 문서의 수
-    idf.length = vocab.size;
+    idf.length = bow[0].length;
     idf.fill(0);
     
     // idf 구하기
@@ -112,34 +92,18 @@ function get_idf(bow, vocab){
 function get_tfidf(bow, idf){
     // tfidf 구하기
     let tfidf = [];
-    let tfidf_obj = {};
-    let sum = 0;
     
-    for(let i in bow){// 문서 개수만큼
+    for(let i in bow){
         let tfidf_temp = [];
+        tfidf_temp.length = bow[0].length;
+        tfidf_temp.fill(0);
         
-        for(let j in bow[i].bow){
-            let t = bow[i].bow[j].value * idf[bow[i].bow[j].index];
-            let pair ={
-                index: bow[i].bow[j].index,
-                value: t
-            };
-            tfidf_temp.push(pair);
+        for(let j in bow[0]){
+            tfidf_temp[j] = bow[i][j] * idf[j];
         }
         
-        // tfidf index 내림차순 정렬    
-        tfidf_temp.sort(function(a, b) {
-            return a.index - b.index;
-        });
-        
-        tfidf_obj = {
-            id: bow[i].id,
-            tfidf: tfidf_temp
-        };
-        sum += tfidf_temp.length;
-        tfidf.push(tfidf_obj);
+        tfidf.push(tfidf_temp);
     }
-    
     console.log('TF-IDF : ', tfidf);
     
     return tfidf;
@@ -148,19 +112,13 @@ function get_tfidf(bow, idf){
 function cosine_similarity(tfidf){
     //0번 문서와 다른 모든 문서를 비교해서 코사인 유사도를 구함
     let cos_sim = [];
-    let normalized_zero = normalize(tfidf[0].tfidf);
+    let normalized_zero = normalize(tfidf[0]);
     
-    for(let i in tfidf){// 전체 문서에 대해
+    for(let i in tfidf){
         let scalar_product = 0;
-        for(let j in tfidf[0].tfidf){// 0번 문서의 tfidf 개수만큼
-            for(let k in tfidf[i].tfidf){// i번 문서의 tfidf 개수만큼
-
-                // 0번 벡터와 i번 벡터의 스칼라곱
-                if(tfidf[0].tfidf[j].index === tfidf[i].tfidf[k].index){
-                    scalar_product += tfidf[0].tfidf[j].value * tfidf[i].tfidf[k].value;
-                    break;
-                }
-            }
+        for(let j in tfidf[i]){
+            // 0번 벡터와 i번 벡터의 스칼라곱
+            scalar_product += tfidf[0][j] * tfidf[i][j];
         }
         
         let cos_sim_temp = 0;
@@ -170,12 +128,12 @@ function cosine_similarity(tfidf){
         }
         else{
             // 분자가 0이 아니면 코사인 유사도 공식 사용
-            cos_sim_temp = scalar_product / (normalized_zero * normalize(tfidf[i].tfidf));
+            cos_sim_temp = scalar_product / (normalized_zero * normalize(tfidf[i]));
             cos_sim_temp = Number(cos_sim_temp.toFixed(5));
         }
         
         let cos_sim_obj = {
-            id: tfidf[i].id,
+            index: i,
             similarity: cos_sim_temp
         };
         cos_sim.push(cos_sim_obj);
@@ -186,18 +144,12 @@ function cosine_similarity(tfidf){
         return b.similarity - a.similarity;
     });
     
-    // 상위 5개의 bid만 추출
-    let top5_cos_sim_bid = [];
-    for(let i=1; i<6; i++){
-        console.log(cos_sim[i]);
-        top5_cos_sim_bid.push(cos_sim[i].bid);
-    }
+    console.log('cosine_similarity : ', cos_sim);
     
-    return top5_cos_sim_bid;
+    return cos_sim;
 }
 
 function similarity_test(document){
-    console.time('time');
     // 문서 토큰화
     let tokenized_document = tokenizer(document);
     console.log('tokenized_document : ', tokenized_document);
@@ -208,48 +160,23 @@ function similarity_test(document){
     let bow = result[1];
     
     // 모든 단어의 idf 구하기
-    let idf = get_idf(bow, vocab);
+    let idf = get_idf(bow);
     
     // 모든 문서의 tfidf 구하기
     let tfidf = get_tfidf(bow, idf);
     
     // 0번 문서와 나머지 문서의 유사도 검사
     let cos_sim = cosine_similarity(tfidf);
-    console.timeEnd('time');
-}
-
-function similarity_test_token(tokenized_document){
-    console.time('time');
-    // 모든 단어에 index 맵핑
-    let result = build_bag_of_words(tokenized_document);
-    let vocab = result[0];
-    let bow = result[1];
-    
-    // 모든 단어의 idf 구하기
-    let idf = get_idf(bow, vocab);
-    
-    // 모든 문서의 tfidf 구하기
-    let tfidf = get_tfidf(bow, idf);
-    
-    // 0번 문서와 나머지 문서의 유사도 검사
-    let cos_sim = cosine_similarity(tfidf);
-    console.timeEnd('time');
 }
 
 function normalize(vector){
     // 벡터 정규화 공식
     let sum_square = 0;
     for(let i in vector){
-        sum_square += vector[i].value * vector[i].value;
+        sum_square += vector[i] * vector[i];
     }
     
     return Math.sqrt(sum_square);
-}
-
-function load_document_file(path){
-    const fs = require('fs');
-    let readData = fs.readFileSync(path);
-    return JSON.parse(readData.toString());
 }
 
 module.exports = {
@@ -259,6 +186,4 @@ module.exports = {
     get_tfidf,
     cosine_similarity,
     similarity_test,
-    similarity_test_token,
-    load_document_file,
 };
